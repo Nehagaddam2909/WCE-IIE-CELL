61ad88e6073523382236428355d57a71a7654019
Nehagaddam2909 <nehagaddam2909@gmail.com>
2023-02-07 21:25:39 +0530

    Changed form UI

--- Components/Register/IdeaSubmission.jsx
+++ Components/Register/IdeaSubmission.jsx
@@ -11,47 +11,53 @@ const IdeaSubmission = () => {
     console.log(visible);
   };
   return (
-    <div className=" flex flex-col w items-center justify-center">
+    <div className=" flex flex-col  items-center justify-center">
       <div className="my-3 tracking-wide font-medium text-[1.5rem]">
         Idea Submission
       </div>
-      <form className="grid grid-cols-1 md:grid-cols-2 space-y-4">
-        <div className="flex items-center justify-evenly  space-x-4">
-          <label>Title:</label>
+      <form className="grid grid-cols-1 md:grid-cols-2 space-x-12 space-y-5">
+        <div className="grid grid-cols-2 items-center space-x-4">
+          <label className="ml-5">Title:</label>
           <input type="text" className="p-1 outline outline-1 bg-gray-50" />
         </div>
-        <div className="flex items-center space-x-4">
+
+        <div className="grid grid-cols-2 items-center space-x-4">
           <label>Name of the leader:</label>
           <input type="text" className="p-1 outline outline-1 bg-gray-50" />
         </div>
-        <div className="flex items-center space-x-4">
+        <div className="grid grid-cols-2 items-center space-x-4">
           <label>Select type:</label>
+          <div className="flex space-x-4">
+            <div className="flex space-x-2">
+              <input
+                type="radio"
+                name="type"
+                id="rd1"
+                className="p-1 bg-gray-50"
+                value={"Student"}
+              />
+              <label htmlFor="id1">Student</label>
+            </div>
+            <div className="flex space-x-2">
+              <input
+                type="radio"
+                name="type"
+                id="id2"
+                className="p-1  bg-gray-50"
+                value={"Faculty"}
+              />
 
-          <input
-            type="radio"
-            name="type"
-            id="rd1"
-            className="p-1 bg-gray-50"
-            value={"Student"}
-          />
-          <label htmlFor="id1">Student</label>
-          <input
-            type="radio"
-            name="type"
-            id="id2"
-            className="p-1  bg-gray-50"
-            value={"Faculty"}
-          />
-
-          <label htmlFor="id2">Faculty</label>
+              <label htmlFor="id2">Faculty</label>
+            </div>
+          </div>
         </div>
-        <div className="flex items-center space-x-4">
+        <div className="grid grid-cols-2 items-center space-x-4">
           <label>Mobile No:</label>
           <input type="text" className="p-1 outline outline-1 bg-gray-50" />
         </div>
-        <div className="flex items-center  space-x-4">
+        <div className="grid grid-cols-2 items-center  space-x-4">
           <label>Department:</label>
-          <select>
+          <select className="p-2">
             <option>Electrical</option>
             <option>Electrical</option>
             <option>Electrical</option>
@@ -60,27 +66,27 @@ const IdeaSubmission = () => {
             <option>Electrical</option>
           </select>
         </div>
-        <div className="flex items-center space-x-4">
+        <div className="grid grid-cols-2 items-center space-x-4">
           <label>Program:</label>
-          <select>
+          <select className="p-2">
             <option>UG</option>
             <option>PG</option>
           </select>
         </div>
-        <div className="flex items-center  space-x-4">
+        <div className="grid grid-cols-2 items-center  space-x-4">
           <label>Year:</label>
-          <select>
+          <select className="p-2">
             <option>First Year</option>
             <option>Second Year</option>
             <option>Third Year</option>
             <option>Final Year</option>
           </select>
         </div>
-        <div className="flex items-center space-x-4">
+        <div className="grid grid-cols-2 items-center space-x-4">
           <label>Name of other team members/guide:</label>
           <input type="text" className="p-1 outline outline-1 bg-gray-50" />
         </div>
-        <div className="flex items-center space-x-4">
+        <div className="grid grid-cols-2 items-center space-x-4">
           <label>Abstract/Report:</label>
           <input
             type="file"
@@ -96,7 +102,7 @@ const IdeaSubmission = () => {
             desc={ele.desc}
           ></Question>
         ))}
-        <div className="flex justify-center">
+        <div className="grid grid-cols-2 justify-center">
           <button
             type="submit"
             className="p-2 rounded-xl w-[8rem] border bg-primary text-white"
--- Components/Register/Question.jsx
+++ Components/Register/Question.jsx
@@ -1,15 +1,15 @@
 const Question = ({ logo, title, desc }) => {
   return (
-    <div className="flex space-x-4">
-      <label>{title}</label>
+    <div className="grid grid-cols-2 space-x-4">
+      <label>{title} &nbsp;
       <div className="tooltip1">
         <img src={logo} className="pr-2" />
 
         <div className="tooltiptext">{desc}</div>
-      </div>
+      </div></label>
       <textarea
         accept=".pdf,.docx"
-        className="p-1 outline outline-1 bg-gray-50"
+        className="p-1 outline outline-1 bg-gray-50 h-10"
       />
     </div>
   );
