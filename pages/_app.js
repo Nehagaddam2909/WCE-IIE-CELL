3cfb57ceb9f63d801c38fe1bfaf0b7909d9f73e7
Nehal Ughade <nehalughade1221@gmail.com>
2023-02-11 21:34:15 +0530

    temp

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '@/styles/globals.css'
-import Navbar from '@/Components/Navbar/Navbar'
+import Navbar from '@/components/Navbar/Navbar'
 
 export default function App({ Component, pageProps }) {
   return <>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      // boxShadowColor: {
+      //   'gdsc-gradient': 'linear-gradient(90deg, ##4285f4 0%, #db4437 20%, #f4b400 40%, #0F9D58 60%, ##4285f4 80%)',
+      //   'gdsc-gradient-reverse': 'linear-gradient(90deg, ##4285f4 0%, #0F9D58 20%, #f4b400 40%, #db4437 60%, ##4285f4 80%)',
+      // },
       colors: {
-        primary: '#FA183E',
+        'gdsc-blue': '#4285f4',
+        'gdsc-red': '#db4437',
+        'gdsc-yellow': '#f4b400',
+        'gdsc-green': '#0f9d58',
+        'gdsc-gradient': 'linear-gradient(90deg, ##4285f4 0%, #db4437 20%, #f4b400 40%, #0F9D58 60%, ##4285f4 80%)',
+        'gdsc-gradient-reverse': 'linear-gradient(90deg, ##4285f4 0%, #0F9D58 20%, #f4b400 40%, #db4437 60%, ##4285f4 80%)',
+
+      },
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+      },
+      boxShadow: {
+        'contact': 'rgba(66, 133, 244,1)0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;',
+        'footer': '0px 5px 12px rgba(0, 0, 0, 0.35)'
+      },
+      padding: {
+        '3.5': '0.775rem',
+      },
+      height: {
+        '80vh': '41',
+        '50vh': '23rem',
+        '45vh': '21.5rem',
       }
     },
   },
